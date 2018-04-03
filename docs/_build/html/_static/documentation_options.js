12d4e74a49a61e08723a71a89a3e881883f334fe
Andres Asensio Ramos <aasensio@iac.es>
2018-04-03 20:00:03 +0100

    More cleaning

--- docs/_build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '2.0beta1',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
