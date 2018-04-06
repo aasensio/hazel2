53965aec06f0aea6acaecde18f02d79cc7e00f98
Andres Asensio Ramos <aasensio@iac.es>
2018-04-06 12:52:35 +0100

    Fixed tests again

--- test/test_single_syn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import hazel
-
-def test_single_syn():
-    # Test a single inversion in non-iterator mode
-    mod = hazel.Model('conf_single.ini', working_mode='synthesis')
-    mod.synthesize()
-
-    assert mod.spectrum['spec1'].stokes.shape == (4,150), "incorrect dimensions in synthesis"
