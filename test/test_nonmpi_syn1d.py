ef020605a4f535744a10d087d0f53d6723c3cbb0
Andres Asensio Ramos <aasensio@iac.es>
2018-10-24 12:04:31 +0100

    Removed matplotib which was breaking travis for python 3.6

--- test/test_nonmpi_syn1d.py
+++ test/test_nonmpi_syn1d.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as pl
 import hazel
 import h5py
 
--- test/test_single_syn.py
+++ test/test_single_syn.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as pl
 import hazel
 
 def test_single_syn():
